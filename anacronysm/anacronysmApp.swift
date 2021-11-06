89165b584e7eda7ed4648d45e177af920143c2c6
LucasMPS <lucas.poncem@gmail.com>
2021-11-06 13:15:29 -0300

    Initial Commit

---
+++ anacronysm/anacronysmApp.swift
@@ -0,0 +1,17 @@
+//
+//  anacronysmApp.swift
+//  anacronysm
+//
+//  Created by Lucas Ponce on 06-11-21.
+//
+
+import SwiftUI
+
+@main
+struct anacronysmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
